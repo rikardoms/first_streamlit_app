de6f0c42861393de4bad7101b63d9c3e264cb46d
rikardoms <rikardoms@gmail.com>
2022-05-20 23:36:48 -0400

    Update streamlit_app.py
    Adding multiselect to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
