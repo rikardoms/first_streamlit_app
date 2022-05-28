74a74af6e604828c035ff6a71971314757861fbd
rikardoms <rikardoms@gmail.com>
2022-05-28 10:01:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,14 @@ from pymongo import MongoClient
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
 def init_connection():
     print(st.secrets["mongo"])
     return pymongo.MongoClient(st.secrets["mongo"])
 
 # Pull data from the collection.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def get_data():
-    db = client.mydb
-    items = db.mycollection.find()
-    items = list(items)  # make hashable for st.experimental_memo
-    return items
+
+
   
 client = init_connection()
 items = get_data()
